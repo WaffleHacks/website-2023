8cdc050b3443f097a26d43086286b65bf20bc88c
Alex Krantz <alex@krantz.dev>
2022-11-25 13:02:45 -0800

    Use import aliases

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,7 @@
-import '../styles/globals.css';
-import 'normalize.css/normalize.css';
 import type { AppProps } from 'next/app';
+
+import 'normalize.css/normalize.css';
+import '@/styles/globals.css';
 
 export default function App({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
