2781e25f1d0f980d5de8ad811bb1c4a7e8d0dd1c
Alex Krantz <alex@krantz.dev>
2023-02-09 15:02:06 -0800

    Add hackclub EIN to footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -2,8 +2,8 @@ import Link from 'next/link';
 
 const Footer = () => {
   return (
-    <div id="footer" className="flex flex-col items-center py-4">
-      <span className="mb-4">© WaffleHacks 2023</span>
+    <div id="footer" className="flex flex-col items-center py-4 space-y-4">
+      <span>© WaffleHacks 2023</span>
       <div className="flex flex-row gap-4 flex-wrap px-8 justify-around">
         <Link href="/">Home</Link>
         <Link href="/privacy" target="_blank">
@@ -15,6 +15,10 @@ const Footer = () => {
         <Link href="https://static.mlh.io/docs/mlh-code-of-conduct.pdf" rel="noreferrer" target="_blank">
           MLH Code of Conduct
         </Link>
+      </div>
+      <div className="text-gray-800">
+        <p>Fiscally sponsored by The Hack Foundation</p>
+        <p>Non-profit EIN: 81-2908499</p>
       </div>
     </div>
   );
